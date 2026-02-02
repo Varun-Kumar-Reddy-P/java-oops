aed477988da81d29807dd60ddf360c4e184ae074
Varun-Kumar-Reddy-P <pvarunkreddy@gmail.com>
2026-02-02 21:56:03 +0530

    added default constructor example

---
+++ DefaultConstructor.java
@@ -0,0 +1,12 @@
+class DefaultConstructor{
+    
+    DefaultConstructor(){
+        System.out.println("Constructor executed");
+    }
+    
+    public static void main(String []args){
+        DefaultConstructor obj = new DefaultConstructor();
+    }
+}
+
+
